aa17dec26cb45e2933e59444e476be0e82c365bc
enoch B <henokt828@gmail.com>
2025-10-23 22:30:28 +0300

    feat: Add Google Play Services for Maps and Location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,13 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 }
-
 dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
